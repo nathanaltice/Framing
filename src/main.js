2a6a3ae027bed1e31aa5ff00ea46150890274121
Nathan Altice <naltice@ucsc.edu>
2024-03-08 13:36:57 -0800

    phaser update + new scale mode

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Nathan Altice
 // Created: 5/23/21
-// Updated: 6/1/23
-// Phaser ScaleManager, CSS, etc.
+// Updated: 3/7/24
+// Phaser ScaleManager, CSS, fullscreen toggle, etc.
 
 let config = {
     parent: 'gameView',
@@ -9,16 +9,17 @@ let config = {
     width: 800,
     height: 600,
     // Try uncommenting different combinations of mode & autoCenter properties
-    // Note: Vertical centering requires a parent <div> that is sized
+    // Note: Vertical centering requires a parent <div> that is sized (see index.html + scales.css)
     // Also, in most cases, Phaser.Scale.FIT is your best bet
     scale: {
-        //mode: Phaser.Scale.NONE,
+        mode: Phaser.Scale.NONE,
         //mode: Phaser.Scale.FIT,
         //mode: Phaser.Scale.ENVELOP,
         //mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
         //mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
+        //mode: Phaser.Scale.EXPAND,
         //mode: Phaser.Scale.RESIZE,
-        //autoCenter: Phaser.Scale.NO_CENTER,
+        autoCenter: Phaser.Scale.NO_CENTER,
         //autoCenter: Phaser.Scale.HORIZONTALLY,
         //autoCenter: Phaser.Scale.VERTICALLY,
         //autoCenter: Phaser.Scale.CENTER_BOTH
